6726067320060a947e75762a8f16f90c818fe6b8
Manolis <73672732+Manolisp52@users.noreply.github.com>
2020-12-10 13:10:35 +0200

    Update Webcam detection.py

--- Webcam detection.py
+++ Webcam detection.py
@@ -9,7 +9,7 @@ cap = cv2.VideoCapture(0)
 # cap.set(10,150)
 
 classNames= []
-classFile = 'coco.names'
+classFile = 'labels.txt'
 with open(classFile,'rt') as f:
     classNames = f.read().rstrip('\n').split('\n')
 
@@ -24,8 +24,8 @@ net.setInputMean((127.5, 127.5, 127.5))
 net.setInputSwapRB(True)
 
 while True:
-    success,img = cap.read()
-    classIds, confs, bbox = net.detect(img,confThreshold=thres)
+    success,frame = cap.read()
+    classIds, confs, bbox = net.detect(frame,confThreshold=thres)
     bbox = list(bbox)
     confs = list(np.array(confs).reshape(1,-1)[0])
     confs = list(map(float,confs))
@@ -39,11 +39,11 @@ while True:
         i = i[0]
         box = bbox[i]
         x,y,w,h = box[0],box[1],box[2],box[3]
-        cv2.rectangle(img, (x,y),(x+w,h+y), color=(0, 255, 0), thickness=2)
-        cv2.putText(img,classNames[classIds[i][0]-1].upper(),(box[0]+10,box[1]+30),
+        cv2.rectangle(frame, (x,y),(x+w,h+y), color=(0, 255, 0), thickness=2)
+        cv2.putText(frame,classNames[classIds[i][0]-1].upper(),(box[0]+10,box[1]+30),
                     cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
 
-    cv2.imshow("Output",img)
+    cv2.imshow("Output",frame)
     cv2.waitKey(1)
 
 
